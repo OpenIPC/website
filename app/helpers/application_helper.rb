48c7c5983ae07beaa3faeaf1b23263ef43fdc906
Paul Philippov <themactep@gmail.com>
2022-07-10 20:59:45 -0400

    rewrite partition calculator.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   def ipaddr_pattern
     '^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$'
   end
+
+  def partition_names
+    %w[boot env kernel rootfs rootfs_data]
+  end
+  def partition_sizes
+    %w[512 64 2048 5120 -]
+  end
 end
