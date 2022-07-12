c5140f33e8399be85d69a71fb6bbc927cc6e4d98
Paul Philippov <themactep@gmail.com>
2022-07-12 03:59:42 -0400

    refactor js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,18 @@
-//
+function $(n) {
+    return document.querySelector(n);
+}
+
+function $$(n) {
+    return document.querySelectorAll(n);
+}
+
+window.onload = (event) => {
+    $$('a[href^="http"]').forEach(el => {
+        el.target = "_blank";
+        el.classList.add('external-link');
+    });
+    $$('a[rel^="external"]').forEach(el => {
+        el.target = "_blank";
+        el.classList.add('external-link');
+    });
+};
