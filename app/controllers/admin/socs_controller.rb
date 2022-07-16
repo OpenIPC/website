43153b3ffb49aa8d9a9a516488e9d04f69950ee4
Paul Philippov <themactep@gmail.com>
2022-07-15 23:48:12 -0400

    add toolchain_filename to permitted params

--- app/controllers/admin/socs_controller.rb
+++ app/controllers/admin/socs_controller.rb
@@ -31,9 +31,8 @@ class Admin
     private
       def permitted_params
         params.require(:soc).permit(
-          :model, :vendor_id, :version, :status,
-          :load_address, :sdk, :kernel, :uboot_filename,
-          :linux_filename, :notes
+          :model, :vendor_id, :version, :status, :load_address, :sdk, :kernel,
+          :uboot_filename, :linux_filename, :toolchain_filename, :notes
         )
       end
   end
