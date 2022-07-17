bef78beb800e02cbdd41134f08532628b7a41ac2
Paul Philippov <themactep@gmail.com>
2022-07-17 05:02:32 -0400

    calculate hex size for nor32. erase as much as you copy

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -6,8 +6,8 @@ class Camera
 
   attr_accessor :soc_id, :needs_instruction, :flash_type, :sd_card_slot, :network_interface, :camera_ip_address, :server_ip_address
 
-  validates :soc_id,      presence: true
-  validates :flash_type,  presence: true
+  validates :soc_id, presence: true
+  validates :flash_type, presence: true
   validates :camera_ip_address, format: { with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex) }
   validates :server_ip_address, format: { with: Regexp.union(Resolv::IPv4::Regex, Resolv::IPv6::Regex) }
 
@@ -24,7 +24,18 @@ class Camera
   end
 
   def flash_size_hex
-    @flash_size_hex ||= @flash_type.eql?("nor16m") ? "0x1000000" : "0x800000"
+    @flash_size_hex ||= case @flash_type
+                        when "nor8m"
+                          "0x800000"
+                        when "nor16m"
+                          "0x1000000"
+                        when "nor32m"
+                          "0x2000000"
+                        # when "nand"
+                        #   "0x800000"
+                        else
+                          "0x800000"
+                        end
   end
 
   # if @camera.flash_type.eql?("nor16m")
