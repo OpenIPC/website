9468a6f047e42e11623b0d10a23119d4d89541df
Paul Philippov <themactep@gmail.com>
2022-08-04 03:38:21 -0400

    log validation errors

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  after_validation :log_errors, if: Proc.new { |m| m.errors }
+
+  def log_errors
+    Rails.logger.debug self.errors.full_messages.join("\n")
+  end
 end
