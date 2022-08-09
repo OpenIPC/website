4f672b2988e309078efe80bb6689e94691a3fb1e
Paul Philippov <themactep@gmail.com>
2022-08-08 23:47:30 -0400

    ask confirmation on warning and danger buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,17 @@ function $$(n) {
     return document.querySelectorAll(n);
 }
 
+// For .warning and .danger buttons, ask confirmation on action.
+$$('.btn-danger, .btn-warning, .confirm').forEach(el => {
+    // for input, find its parent form and attach listener to it submit event
+    if (el.nodeName === "INPUT") {
+        while (el.nodeName !== "FORM") el = el.parentNode
+        el.addEventListener('submit', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
+    } else {
+        el.addEventListener('click', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
+    }
+});
+
 window.onload = (event) => {
     $$('a[href^="http"]').forEach(el => {
         el.target = "_blank";
