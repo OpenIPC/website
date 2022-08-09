fb74a883d5cd92a0f37b0d234ae57a4a3e2caac9
Paul Philippov <themactep@gmail.com>
2022-08-09 00:46:05 -0400

    ask confirmation on warning and danger button actions.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,24 +6,20 @@ function $$(n) {
     return document.querySelectorAll(n);
 }
 
-// For .warning and .danger buttons, ask confirmation on action.
-$$('.btn-danger, .btn-warning, .confirm').forEach(el => {
-    // for input, find its parent form and attach listener to it submit event
-    if (el.nodeName === "INPUT") {
-        while (el.nodeName !== "FORM") el = el.parentNode
-        el.addEventListener('submit', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
-    } else {
-        el.addEventListener('click', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
-    }
-});
-
 window.onload = (event) => {
-    $$('a[href^="http"]').forEach(el => {
+    $$('a[href^="http"], a[rel^="external"]').forEach(el => {
         el.target = "_blank";
         el.classList.add('external-link');
     });
-    $$('a[rel^="external"]').forEach(el => {
-        el.target = "_blank";
-        el.classList.add('external-link');
+
+    // For .warning and .danger buttons, ask confirmation on action.
+    $$('.btn-danger, .btn-warning, .confirm').forEach(el => {
+        // for input or button, find parent form and attach listener to its submit event
+        if (el.nodeName === "INPUT" || el.nodeName === "BUTTON") {
+            while (el.nodeName !== "FORM") el = el.parentNode
+            el.addEventListener('submit', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
+        } else {
+            el.addEventListener('click', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
+        }
     });
 };
