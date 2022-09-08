3aeec0f4710baa511317a3b542eb04cc1f7716c0
Paul Philippov <themactep@gmail.com>
2022-09-08 07:56:42 -0400

    add a webhook for github actions feedback

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,10 +2,17 @@ class GithubController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    permitted_params = params.permit(:platform, :release, :toolchain)
-    Soc.where(family: permitted_params[:platform]).each do |s|
-      s.update toolchain: permitted_params[:toolchain]
+    permitted_params = params.permit(:platform, :release, :token, :toolchain)
+    family = permitted_params[:platform].upcase
+    toolchain_filename = permitted_params[:toolchain]
+    token = permitted_params[:token]
+    if token.eql?(Rails.application.credentials.dig(:github, :token))
+      Soc.where(family: family).each do |s|
+        s.update toolchain_filename: toolchain_filename
+      end
+      head :ok
+    else
+      head 403
     end
-    head :ok
   end
 end
