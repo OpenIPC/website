aa7bc7c8fec0023f622b901821a97e8166913de3
Paul Philippov <themactep@gmail.com>
2023-03-06 17:23:37 -0500

    use single quotes where no interpolation needed.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,7 @@ function $$(n) {
 
 window.onload = (event) => {
     $$('a[href^="http"], a[rel^="external"]').forEach(el => {
-        el.target = "_blank";
+        el.target = '_blank';
         el.classList.add('external-link');
     });
 
@@ -17,7 +17,7 @@ window.onload = (event) => {
     }
 
     $$('span[data-timestamp]').forEach(el => {
-        const ts = el.dataset["timestamp"];
+        const ts = el.dataset['timestamp'];
         let date = new Date(ts * 1000);
         el.textContent = date.toLocaleString([], { dateStyle: 'medium', timeStyle: 'short' });
     });
@@ -25,11 +25,11 @@ window.onload = (event) => {
     // For .warning and .danger buttons, ask confirmation on action.
     $$('.btn-danger, .btn-warning, .confirm').forEach(el => {
         // for input or button, find parent form and attach listener to its submit event
-        if (el.nodeName === "INPUT" || el.nodeName === "BUTTON") {
+        if (el.nodeName === 'INPUT' || el.nodeName === 'BUTTON') {
             while (el.nodeName !== "FORM") el = el.parentNode
-            el.addEventListener('submit', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
+            el.addEventListener('submit', ev => (!confirm('Are you sure?')) ? ev.preventDefault() : null)
         } else {
-            el.addEventListener('click', ev => (!confirm("Are you sure?")) ? ev.preventDefault() : null)
+            el.addEventListener('click', ev => (!confirm('Are you sure?')) ? ev.preventDefault() : null)
         }
     });
 };
