92e5870ef1fd6775df84e849b0aafc02c6c954d6
Paul Philippov <themactep@gmail.com>
2023-04-22 19:02:56 -0400

    discard jobs on deserialization error

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,7 +3,6 @@ class ApplicationJob < ActiveJob::Base
   # retry_on ActiveRecord::Deadlocked
 
   # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
-
+  discard_on ActiveJob::DeserializationError
   discard_on ActiveRecord::RecordNotFound
 end
