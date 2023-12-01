4294cca569e1ab9baed523dee45dfc17c25af3be
Paul Philippov <themactep@gmail.com>
2023-12-01 09:49:10 -0500

    add german translation

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,7 +2,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
 
 # Permitted locales available for the application
-I18n.available_locales = [:en, :ru]
+I18n.available_locales = [:en, :de, :ru, :zh]
 
 # Set default locale to something other than :en
 I18n.default_locale = :en
